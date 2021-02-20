bb211bf4d60d2084ba12c074f13d1a785782d9ab
PJ Fanning <pjfanning@users.noreply.github.com>
2021-02-20 19:25:18 +0000

    using paranamer fixes some tests

--- src/main/scala-2.13/com/fasterxml/jackson/module/scala/introspect/JavaParameterIntrospector.scala
+++ src/main/scala-2.13/com/fasterxml/jackson/module/scala/introspect/JavaParameterIntrospector.scala
@@ -2,17 +2,23 @@ package com.fasterxml.jackson.module.scala.introspect
 
 import java.lang.reflect.{Constructor, Field, Method, Parameter}
 
+import com.thoughtworks.paranamer.{BytecodeReadingParanamer, CachingParanamer}
+
 private[introspect] object JavaParameterIntrospector {
 
+  private val paranamer = new CachingParanamer(new BytecodeReadingParanamer)
+
   def getCtorParamNames(ctor: Constructor[_]): IndexedSeq[String] = {
-    ctor.getParameters.map(_.getName).toIndexedSeq
+    paranamer.lookupParameterNames(ctor, false).toIndexedSeq
   }
 
   def getMethodParamNames(mtd: Method): IndexedSeq[String] = {
-    mtd.getParameters.map(_.getName).toIndexedSeq
+    paranamer.lookupParameterNames(mtd, false).toIndexedSeq
   }
 
-  def getFieldName(field: Field): String = field.getName
+  def getFieldName(field: Field): String = {
+    paranamer.lookupParameterNames(field, false).headOption.getOrElse(None.orNull)
+  }
 
   def getParameterName(parameter: Parameter): String = parameter.getName
 }
